d72c95593017030cf8e3687b7c3d81f4c01a771b
FordProject <ajakahn1@gmail.com>
2025-05-01 10:51:29 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -45,8 +45,8 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pyrogram",
     download_url="https://github.com/pyrogram/pyrogram/releases/latest",
-    author="Dan",
-    author_email="dan@pyrogram.org",
+    author="Ford",
+    author_email="kontollo@gmail.conm",
     license="LGPLv3",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
